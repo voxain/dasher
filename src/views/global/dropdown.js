a7f3720179a2992f7754d2da94c9724c9a1e081e
rmcproductions <info@rmcprod.me>
2019-08-20 16:29:37 +0200

    commit message here

--- src/views/global/dropdown.js
+++ src/views/global/dropdown.js
@@ -2,4 +2,11 @@ $(document).ready(() => {
     $('.header-dropdown').on('click', e => {
         $('#drop-machines').toggleClass('visible');
     });
+
+    function modal(options){
+        var parser = new DOMParser();
+        var htmlDoc = parser.parseFromString(`
+        
+        `, 'text/xml');
+    }
 });
